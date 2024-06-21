1dbef478924c504409ca36a67cc0c9ec92059d47
Yamemik <kuancarlos@mail.ru>
2024-06-21 15:55:27 +0500

    + поиск экскурсии

--- Controls/TourUserControl.xaml.cs
+++ Controls/TourUserControl.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Xps;
 
 
 namespace TravelAgency.Controls
@@ -40,21 +41,30 @@ namespace TravelAgency.Controls
 
         private void TboxSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
-            UpdateTours();
+            string name = county_name.Text;
+            UpdateTours(name);
         }
 
-        private void UpdateTours()
+        private void UpdateTours(string name)
         {
+            using (TravelDBContext db = new())
+            {
+                // запрос на получение всех категорий и связанных с ними продуктов
+                IQueryable<Excursion>? ent = db.Excursions
+                    .Include(c => c.Country)
+                    .Where(c => c.Country.Name.StartsWith(name));
 
+                DataGridEx.ItemsSource = ent.ToList<Excursion>();
+            }
         }
         private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            UpdateTours();
+            //UpdateTours();
         }
 
         private void ComboPrice_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            UpdateTours();
+            //UpdateTours();
         }
 
         private void btnAbout_click(object sender, RoutedEventArgs e)
